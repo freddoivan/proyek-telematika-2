d0373ca21804db7ee2e6b4e3e0db0e6a2ca01d18
Rendi <rendicrew96@gmail.com>
2024-01-06 20:39:11 +0700

    update

--- FrontEnd/src/lib/axios-mock.ts
+++ FrontEnd/src/lib/axios-mock.ts
@@ -2,7 +2,7 @@ import { QueryFunction } from '@tanstack/react-query';
 import axios from 'axios';
 
 /** Add NEXT_PUBLIC_MOCK_DEPLOYMENT_URL to your production deployment on vercel! */
-const baseURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const baseURL = `https://${process.env.API_URL}`
 
 export const apiMock = axios.create({
   baseURL,
--- FrontEnd/src/lib/pusher.tsx
+++ FrontEnd/src/lib/pusher.tsx
@@ -4,7 +4,7 @@ export const pusherClient = new PusherClient(
   'getfitkey',
   {
     cluster: 'eu',
-    wsHost: process.env.NEXT_PUBLIC_SOCKET_URL,
+    wsHost: process.env.SOCKET_URL,
     wsPort: 6001,
     forceTLS: false,
     disableStats: false,
