3af2568c423c02fc5e4f323a747d7d9815d62d1d
Rendi <rendicrew96@gmail.com>
2024-01-06 20:33:49 +0700

    update endpoint

--- FrontEnd/src/lib/axios-mock.ts
+++ FrontEnd/src/lib/axios-mock.ts
@@ -2,11 +2,7 @@ import { QueryFunction } from '@tanstack/react-query';
 import axios from 'axios';
 
 /** Add NEXT_PUBLIC_MOCK_DEPLOYMENT_URL to your production deployment on vercel! */
-const baseURL = process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL
-? `https://${process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL}/api/mock`
-: process.env.NEXT_PUBLIC_VERCEL_URL
-? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-: 'http://localhost:5000/';
+const baseURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 
 export const apiMock = axios.create({
   baseURL,
