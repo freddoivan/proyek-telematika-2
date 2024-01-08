141218d769632e7d896d95983fec1344e429a3c8
Samuel <89601859+samuelsih@users.noreply.github.com>
2024-01-08 15:41:36 +0700

    Update pusher.tsx

--- FrontEnd/src/lib/pusher.tsx
+++ FrontEnd/src/lib/pusher.tsx
@@ -4,7 +4,7 @@ export const pusherClient = new PusherClient(
   'getfitkey',
   {
     cluster: 'eu',
-    wsHost: '159.89.207.232',
+    wsHost: 'apigetfit.duckdns.org',
     wsPort: 6001,
     forceTLS: false,
     disableStats: false,
